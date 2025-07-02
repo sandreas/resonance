2770085653dac35d95d6fc1bd539105850bc7320
sandreas <2050604+sandreas@users.noreply.github.com>
2025-07-02 06:52:48 +0200

    Added NoficationChannel to MainApp

--- app/src/main/java/com/pilabor/resonance/MainApp.kt
+++ app/src/main/java/com/pilabor/resonance/MainApp.kt
@@ -2,16 +2,31 @@ package com.pilabor.resonance
 
 import android.app.Application
 import com.pilabor.resonance.di.appModule
+import com.pilabor.resonance.helper.NotificationHelper
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
 class MainApp: Application() {
+    companion object {
+        private var instance: MainApp? = null
+
+        fun getContext(): MainApp {
+            return instance!!
+        }
+    }
 
     override fun onCreate() {
+        instance = this
+
         super.onCreate()
         startKoin {
             androidContext(this@MainApp)
             modules(appModule)
         }
+        NotificationHelper.createNotificationChannel(this)
     }
+
+
+
+
 }
